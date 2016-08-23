8d0eb9b44e1274c17e9fd6e199ab7fa4527dbe28
P@m3lin <P@m3lin@192.168.1.37>
2016-08-22 23:34:57 -0300

    Borrado de Botones Agregada la logica para mover con el teclado

--- src/views/FormMain.java
+++ src/views/FormMain.java
@@ -18,6 +18,7 @@ import javax.swing.JEditorPane;
 import java.awt.Panel;
 import java.awt.SystemColor;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 
@@ -32,7 +33,7 @@ public class FormMain {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					FormMain window = new FormMain();
+					FormMain window = new FormMain();				
 					window.frmVargas.setVisible(true);
 					
 				} catch (Exception e) {
@@ -54,39 +55,10 @@ public class FormMain {
 	 */
 	private void initialize() {
 		frmVargas = new JFrame();
-		frmVargas.getContentPane().addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent arg0) {
-				System.out.println(arg0.getKeyChar());
-			}
-		});
 		frmVargas.setTitle("2048 1.0 Vargas Lucero");
 		frmVargas.setBounds(100, 100, 506, 445);
 		frmVargas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmVargas.getContentPane().setLayout(null);
-		
-		JButton btnNumero = new JButton("2");
-		btnNumero.setBounds(400, 46, 89, 57);
-		frmVargas.getContentPane().add(btnNumero);
-		
-		JButton btnIzq = new JButton("<<<");
-		btnIzq.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				btnNumero.setBounds(btnNumero.getX()-5, btnNumero.getY(), btnNumero.getWidth(), btnNumero.getHeight());
-				
-			}
-		});
-		btnIzq.setBounds(129, 381, 89, 23);
-		frmVargas.getContentPane().add(btnIzq);
-		
-		JButton btnDer = new JButton(">>>");
-		btnDer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				btnNumero.setBounds(btnNumero.getX()+5, btnNumero.getY(), btnNumero.getWidth(), btnNumero.getHeight());
-			}
-		});
-		btnDer.setBounds(228, 381, 89, 23);
-		frmVargas.getContentPane().add(btnDer);
 		
 		JPanel pnl00 = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) pnl00.getLayout();
@@ -96,7 +68,7 @@ public class FormMain {
 		pnl00.setBounds(66, 46, 75, 75);
 		frmVargas.getContentPane().add(pnl00);
 		
-		JLabel lbl00 = new JLabel("0000");
+		JLabel lbl00 = new JLabel("");
 		lbl00.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl00.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl00.add(lbl00);
@@ -107,7 +79,7 @@ public class FormMain {
 		pnl01.setBounds(143, 46, 75, 75);
 		frmVargas.getContentPane().add(pnl01);
 		
-		JLabel lbl01 = new JLabel("0000");
+		JLabel lbl01 = new JLabel("");
 		lbl01.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl01.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl01.add(lbl01);
@@ -118,7 +90,7 @@ public class FormMain {
 		pnl02.setBounds(220, 46, 75, 75);
 		frmVargas.getContentPane().add(pnl02);
 		
-		JLabel lbl02 = new JLabel("0000");
+		JLabel lbl02 = new JLabel("");
 		lbl02.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl02.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl02.add(lbl02);
@@ -129,7 +101,7 @@ public class FormMain {
 		pnl03.setBounds(298, 46, 75, 75);
 		frmVargas.getContentPane().add(pnl03);
 		
-		JLabel lbl03 = new JLabel("0000");
+		JLabel lbl03 = new JLabel("");
 		lbl03.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl03.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl03.add(lbl03);
@@ -140,7 +112,7 @@ public class FormMain {
 		pnl10.setBounds(66, 127, 75, 75);
 		frmVargas.getContentPane().add(pnl10);
 		
-		JLabel lbl10 = new JLabel("0000");
+		JLabel lbl10 = new JLabel("");
 		lbl10.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl10.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl10.add(lbl10);
@@ -151,7 +123,7 @@ public class FormMain {
 		pnl11.setBounds(143, 127, 75, 75);
 		frmVargas.getContentPane().add(pnl11);
 		
-		JLabel lbl11 = new JLabel("0000");
+		JLabel lbl11 = new JLabel("");
 		lbl11.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl11.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl11.add(lbl11);
@@ -162,7 +134,7 @@ public class FormMain {
 		pnl12.setBounds(220, 127, 75, 75);
 		frmVargas.getContentPane().add(pnl12);
 		
-		JLabel lbl12 = new JLabel("0000");
+		JLabel lbl12 = new JLabel("");
 		lbl12.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl12.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl12.add(lbl12);
@@ -173,7 +145,7 @@ public class FormMain {
 		pnl13.setBounds(298, 127, 75, 75);
 		frmVargas.getContentPane().add(pnl13);
 		
-		JLabel lbl13 = new JLabel("0000");
+		JLabel lbl13 = new JLabel("");
 		lbl13.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl13.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl13.add(lbl13);
@@ -184,7 +156,7 @@ public class FormMain {
 		pnl20.setBounds(66, 206, 75, 75);
 		frmVargas.getContentPane().add(pnl20);
 		
-		JLabel lbl20 = new JLabel("0000");
+		JLabel lbl20 = new JLabel("");
 		lbl20.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl20.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl20.add(lbl20);
@@ -195,7 +167,7 @@ public class FormMain {
 		pnl21.setBounds(143, 206, 75, 75);
 		frmVargas.getContentPane().add(pnl21);
 		
-		JLabel lbl21 = new JLabel("0000");
+		JLabel lbl21 = new JLabel("");
 		lbl21.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl21.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl21.add(lbl21);
@@ -206,7 +178,7 @@ public class FormMain {
 		pnl22.setBounds(220, 206, 75, 75);
 		frmVargas.getContentPane().add(pnl22);
 		
-		JLabel lbl22 = new JLabel("0000");
+		JLabel lbl22 = new JLabel("");
 		lbl22.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl22.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl22.add(lbl22);
@@ -217,7 +189,7 @@ public class FormMain {
 		pnl23.setBounds(298, 206, 75, 75);
 		frmVargas.getContentPane().add(pnl23);
 		
-		JLabel lbl23 = new JLabel("0000");
+		JLabel lbl23 = new JLabel("");
 		lbl23.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl23.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl23.add(lbl23);
@@ -228,7 +200,7 @@ public class FormMain {
 		pnl30.setBounds(66, 284, 75, 75);
 		frmVargas.getContentPane().add(pnl30);
 		
-		JLabel lbl30 = new JLabel("0000");
+		JLabel lbl30 = new JLabel("");
 		lbl30.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl30.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl30.add(lbl30);
@@ -239,7 +211,7 @@ public class FormMain {
 		pnl31.setBounds(143, 284, 75, 75);
 		frmVargas.getContentPane().add(pnl31);
 		
-		JLabel lbl31 = new JLabel("0000");
+		JLabel lbl31 = new JLabel("");
 		lbl31.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl31.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl31.add(lbl31);
@@ -250,7 +222,7 @@ public class FormMain {
 		pnl32.setBounds(220, 284, 75, 75);
 		frmVargas.getContentPane().add(pnl32);
 		
-		JLabel lbl32 = new JLabel("0000");
+		JLabel lbl32 = new JLabel("");
 		lbl32.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl32.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl32.add(lbl32);
@@ -261,10 +233,53 @@ public class FormMain {
 		pnl33.setBounds(298, 284, 75, 75);
 		frmVargas.getContentPane().add(pnl33);
 		
-		JLabel lbl33 = new JLabel("0000");
+		JLabel lbl33 = new JLabel("");
 		lbl33.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl33.setFont(new Font("Calibri", Font.PLAIN, 29));
 		pnl33.add(lbl33);
 		
+		JButton btnNewGame = new JButton("Juego Nuevo");
+		btnNewGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Poner en 0 todos los labels y puntaje a 0 tambien.
+			}
+		});
+		btnNewGame.setBounds(143, 370, 162, 23);
+		btnNewGame.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+		          switch (e.getKeyCode()) {
+		            case KeyEvent.VK_LEFT:
+		              moverIzq();
+		              break;
+		            case KeyEvent.VK_RIGHT:
+		              moverDer();
+		              break;
+		            case KeyEvent.VK_DOWN:
+		              moverAbajo();
+		              break;
+		            case KeyEvent.VK_UP:
+		              moverArriba();
+		              break;
+		          }
+			}
+
+			private void moverArriba() {
+				
+			}
+
+			private void moverAbajo() {
+			}
+
+			private void moverDer() {
+			}
+
+			private void moverIzq() {
+			}
+		});
+		
+		frmVargas.getContentPane().add(btnNewGame);
+		
 	}
+	
+	
 }
