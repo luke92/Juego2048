4e2de91ba4eb2c5d4d6b0633ddccd076e5ed75a1
Lucas Vargas <lucasjv92@gmail.com>
2016-08-24 23:54:35 -0300

    Creacion de invariantes en casillero y grilla

--- src/controladores/Casillero.java
+++ src/controladores/Casillero.java
@@ -6,6 +6,28 @@ public class Casillero
 	
 	public Casillero(Integer n)
 	{
+		invariante(n);
 		numero = n;
+	}
+	
+	private void invariante(Integer n)
+	{
+		if(n == null) lanzarExcepcion("No puede ser null el numero");
+		if(!esPotenciaDe2(n)) lanzarExcepcion("El numero no es una potencia de 2 positiva");
+	}
+	
+	private boolean esPotenciaDe2(int n)
+	{
+		if(n == 0) return true;
+		return ((n & (n - 1)) == 0);
+	}
+	
+	private void lanzarExcepcion(String error)
+	{
+		throw new RuntimeException(error);
+	}
+	public Integer valor()
+	{
+		return this.numero;
 	}
 }
--- src/controladores/Grilla.java
+++ src/controladores/Grilla.java
@@ -6,6 +6,21 @@ public class Grilla
 	
 	public Grilla(int filas, int columnas)
 	{
+		invariante(filas,columnas);
 		casilleros = new Casillero[filas][columnas];
+		inicializarCasilleros();
+	}
+	
+	private void invariante(int filas, int columnas)
+	{
+		if(filas < 3 || columnas < 3)
+			throw new RuntimeException("La cantidad de filas o columnas no puede ser menor a 3");
+	}
+	
+	private void inicializarCasilleros()
+	{
+		for(int i = 0; i < casilleros.length; i++)
+			for(int j = 0; j < casilleros[i].length; i++)
+				casilleros[i][j] = new Casillero(0);
 	}
 }
