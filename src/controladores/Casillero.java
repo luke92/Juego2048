85722b595939902a80a5c5d77490eb3d6be75039
Lucas Vargas <lucasjv92@gmail.com>
2016-08-24 23:23:40 -0300

    Creada la clase Grilla.java y Casillero.java

---
+++ src/controladores/Casillero.java
@@ -0,0 +1,11 @@
+package controladores;
+
+public class Casillero 
+{
+	private Integer numero;
+	
+	public Casillero(Integer n)
+	{
+		numero = n;
+	}
+}
---
+++ src/controladores/Grilla.java
@@ -0,0 +1,11 @@
+package controladores;
+
+public class Grilla 
+{
+	private Casillero casilleros[][];
+	
+	public Grilla(int filas, int columnas)
+	{
+		casilleros = new Casillero[filas][columnas];
+	}
+}
